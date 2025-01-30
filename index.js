9d8f3667982ceaf443d9424aa9dd7b690d4d77fe
aniket-web-13 <aniketweb13@gmail.com>
2025-01-30 15:25:33 +0530

    delete btn update

--- index.js
+++ index.js
@@ -4,30 +4,72 @@ let makeTask = document.getElementById("makeTask");
 let addBtn = document.getElementById("addBtn");
 let taskShow = document.getElementById("taskShow");
 
-let deleteBtn = document.querySelectorAll("deleteBtn")
+
 
 
 let taskList = [];
 
+// function CreateTask() {
+//     let taskName = makeTask.value;
+//     let task = document.createElement("div");
+//     task.classList.add("task");
+//     task.innerHTML = `  
+//         <form action="">
+//             <input type="checkbox">                                           
+//         </form>
+//         <p>${taskName}</p>    
+//         <button class="deleteBtn" id="delete"><i class="fa-solid fa-trash"></i></button>  
+//     `;
+//     taskList.push(taskShow.appendChild(task));
+//     console.log(taskList);
+
+//     deleteBtn.addEventListener("click", () => {
+//         taskList.slice(0)
+//         console.log(taskIndex);
+
+//       })
+
+// }
+
 function CreateTask() {
     let taskName = makeTask.value;
+    if (!taskName) return;
+
     let task = document.createElement("div");
     task.classList.add("task");
-    task.innerHTML = `  
+    task.innerHTML = `
         <form action="">
-            <input type="checkbox">                                           
+            <input type="checkbox" class="checkbox">                                           
         </form>
         <p>${taskName}</p>    
-        <button class="deleteBtn" id="delete"><i class="fa-solid fa-trash"></i></button>  
+        <button class="deleteBtn"><i class="fa-solid fa-trash"></i></button>  
     `;
-    taskList.push(taskShow.appendChild(task));
+
+    taskShow.appendChild(task);
+    taskList.push(task);
+
+    makeTask.value = "";
+
+    let deleteBtn = task.querySelector(".deleteBtn");
+    deleteBtn.addEventListener("click", () => {
+        task.remove();
+        taskList = taskList.filter(t => t !== task);
+    });
+
+    let checkbox = task.querySelector(".checkbox");
+    checkbox.addEventListener("change", () => {
+        task.classList.toggle("done", checkbox.checked);
+    });
+
     console.log(taskList);
-
-    // deleteBtn.addEventListener("click", () => {
-    //     taskList.slice(0)
-    //     console.log(taskIndex);
-        
-    //   })
-
+    
 }
 
+addBtn.addEventListener("click", CreateTask);
+
+
+makeTask.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        CreateTask();
+    }
+});
